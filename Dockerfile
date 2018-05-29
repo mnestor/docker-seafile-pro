0a9630bb11204985920e855e15c2d1062811ca37
Nestor, Michael P 123613304 <mike.nestor@nasa.gov>
2018-05-29 07:03:14 -0400

    almost there, need nginx to proxy shit

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,27 @@
-FROM alpine:latest
+FROM ubuntu:16.04
 MAINTAINER mnestor
 
 WORKDIR /opt/seafile
 
+RUN apt-get update && apt-get upgrade -y \
+    && apt-get install -y \
+    vim cron wget python nginx \
+    python2.7 libpython2.7 python-setuptools python-imaging python-ldap \
+    python-urllib3 ffmpeg python-pip python-mysqldb python-memcache \
+    python-requests openjdk-8-jre poppler-utils \
+    sqlite3 libreoffice libreoffice-script-provider-python \
+    && pip install boto pillow moviepy \
+    && ln -sf /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java /usr/bin/ \
+    && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-ENV SEAFILE_VER 6.2.9
-RUN wget "https://download.seafile.com/d/6e5297246c/files/?p=/pro/seafile-pro-server_${SEAFILE_VER}_x86-64.tar.gz&dl=1" -O /opt/seafile-pro-server_${SEAFILE_VER}_x86-64.tar.gz
+ENV SEAFILE_VER=6.2.13 SEAFILE_DIR=/opt/seafile FILESERVER_PORT=8082 MYSQL_PORT=3306
+RUN wget "https://download.seafile.com/d/6e5297246c/files/?p=/pro/seafile-pro-server_${SEAFILE_VER}_x86-64_Ubuntu.tar.gz&dl=1" -O /opt/seafile-pro-server_${SEAFILE_VER}_x86-64_Ubuntu.tar.gz
 
-RUN addgroup user && adduser -s /bin/sh -u 999 -h /config -D -G user user && adduser user root
+RUN useradd -u 999 -d /config -m -G root,sudo user
 COPY service_seafile.sh /opt
 COPY crontab /opt
 COPY init /
 RUN chmod 770 /init
 
-# Install packages
-RUN apk update && \
-    apk add --no-cache \
-    py-pip openjdk8-jre bash py-imaging \
-    py-mysqldb shadow poppler-utils \ 
-    py-pyldap py-urllib3 memcached py-requests \
-    && pip install boto python-memcached \
-    && rm -rf /var/cache/apk/*
-
-CMD ["service"]
+CMD ["start"]
 ENTRYPOINT ["/init"]
--- service_seafile.sh
+++ service_seafile.sh
@@ -6,98 +6,126 @@ log_file=/opt/seafile/log
 
 ulimit -n 30000
 
-if [ "$1" == "service" ]; then
-    typeset -i retry_count=0
-    while true
-    do
-        su user -c "echo "" > $log_file"
-        echo "start seafile retry time: " $retry_count >> $log_file
-        /opt/seafile/service_seafile.sh restart >> $log_file 2>&1
-        if [ $(cat $log_file | grep -c failed) -eq 0 ]; then
-            echo "start success... retry time: " $retry_count >> $log_file
-            break
-        fi
-        retry_count=$retry_count+1
-        sleep 5
+function download() {
+  if [ ! -d /opt/seafile/seafile-pro-server-${SEAFILE_VER} ]; then
+    if [ ! -f /opt/seafile-pro-server_${SEAFILE_VER}_x86-64_Ubuntu.tar.gz ]; then
+      wget -q "https://download.seafile.com/d/6e5297246c/files/?p=/pro/seafile-pro-server_${SEAFILE_VER}_x86-64_Ubuntu.tar.gz&dl=1" -O /opt/seafile-pro-server_${SEAFILE_VER}_x86-64_Ubuntu.tar.gz
+    fi
+    rm -rf /opt/seafile/seafile-pro-server-${SEAFILE_VER} seafile-server-latest
+    tar xzf /opt/seafile-pro-server_${SEAFILE_VER}_x86-64_Ubuntu.tar.gz -C /opt/seafile
+  fi
+  chown -R user:user /opt/seafile/seafile-pro-server-${SEAFILE_VER}
+  rm -rf /opt/seafile/seafile-pro-server-${SEAFILE_VER}/seahub/thirdpart/future-0.16.0-py2.7.egg
+}
+
+function stop() {
+  cd /opt/seafile/seafile-server-latest
+  su user -c "./seahub.sh stop"
+  su user -c "./seafile.sh stop"
+}
+
+function start() {
+  if [ ! -d /opt/seafile/seafile-server-latest ]; then
+    do_setup
+  fi
+
+  # see if we need to upgrade and do it
+  do_upgrade
+
+  cd /opt/seafile/seafile-server-latest
+  su user -c "./seafile.sh start"
+  su user -c "./seahub.sh start"
+}
+
+function do_setup() {
+  echo "Validating we have the latest downloaded"
+  download
+  if [ -z "${MYSQL_HOST}" ]; then
+    echo "MYSQL_HOST is unset, querying user"
+    while true; do
+      read -p "Are you using mysql or sqlite?" yn
+      case $yn in
+        mysql)
+          su user -c "./seafile-pro-server-${SEAFILE_VER}/setup-seafile-mysql.sh"
+          break
+          ;;
+        sqlite)
+          su user -c "./seafile-pro-server-${SEAFILE_VER}/setup-seafile.sh"
+          break
+          ;;
+        *)
+          echo "Invalid answer";;
+      esac
     done
-    su user -c "crontab /opt/seafile/crontab"
-    tail -F $log_file
-elif [ "$1" == "start" ]; then
-    cd ./seafile-server-latest
-    su user -c "./seafile.sh start"
-    if [ "$FASTCGI" == "1" ]; then
-      su user -c "SEAFILE_FASTCGI_HOST=0.0.0.0 ./seahub.sh start-fastcgi 8000"
-    else
-      su user -c "./seahub.sh start"
-    fi
-elif [ "$1" == "restart" ]; then
-    cd ./seafile-server-latest
-    su user -c "./seafile.sh restart"
-    if [ "$FASTCGI" == "1" ]; then
-      su user -c "SEAFILE_FASTCGI_HOST=0.0.0.0 ./seahub.sh restart-fastcgi 8000"
-    else
-      su user -c "./seahub.sh restart"
-    fi
-elif [ "$1" == "stop" ]; then
-    cd ./seafile-server-latest
-    su user -c "./seahub.sh stop"
-    su user -c "./seafile.sh stop"
-elif [ "$1" == "setup" ]; then
-    tar xzvf /opt/seafile-pro-server_${SEAFILE_VER}_x86-64.tar.gz -C /opt/seafile
-    chown -R user:user seafile-pro-server-${SEAFILE_VER}
-    rm /opt/seafile/seafile-server-latest
-    ln -s /opt/seafile/seafile-pro-server-${SEAFILE_VER} /opt/seafile/seafile-server-latest
-    cd seafile-pro-server-${SEAFILE_VER}
-    su user -c "./setup-seafile-mysql.sh"
-    /opt/seafile/service_seafile.sh start
-elif [ "$1" == "setup_sqlite" ]; then
-    tar xzvf /opt/seafile-pro-server_${SEAFILE_VER}_x86-64.tar.gz -C /opt/seafile
-    chown -R user:user seafile-pro-server-${SEAFILE_VER}
-    rm /opt/seafile/seafile-server-latest && ln -s /opt/seafile/seafile-pro-server-${SEAFILE_VER} /opt/seafile/seafile-server-latest
-    cd seafile-pro-server-${SEAFILE_VER}
-    su user -c "./setup-seafile.sh"
-    /opt/seafile/service_seafile.sh start
-elif [ "$1" == "upgrade" ]; then
-    CURRENT_VER=$(ls -lah | grep 'seafile-server-latest' | awk -F"seafile-pro-server-" '{print $2}')
-    CURRENT_MAJOR_VER=$(echo $CURRENT_VER | awk -F"." '{print $1"."$2}')
-    NEW_MAJOR_VER=$(echo $SEAFILE_VER | awk -F"." '{print $1"."$2}')
-    if [ $CURRENT_VER == $SEAFILE_VER ]; then
-        echo "You already have the same version installed, exit"
-    else
-        tar xzvf /opt/seafile-pro-server_${SEAFILE_VER}_x86-64.tar.gz -C /opt/seafile
-        rm /opt/seafile/seafile-server-latest && ln -s /opt/seafile/seafile-pro-server-${SEAFILE_VER} /opt/seafile/seafile-server-latest
-        cd ./seafile-pro-server-${SEAFILE_VER}/upgrade
-        echo current_version: $CURRENT_VER
-        echo new_version: ${SEAFILE_VER}
-        if [ $CURRENT_MAJOR_VER == $NEW_MAJOR_VER ]; then
-            echo "maintenance update"
-            ./minor-upgrade.sh
-            rm -rf /opt/seafile/seafile-pro-server-${CURRENT_VER}
-        else
-            echo "Big version update, Please run the matching upgrade script, and then run exit"
-            ls
-            su user
-        fi
-    fi
-elif [ "$1" == "search_update" ]; then
-    cd ./seafile-server-latest
-    /opt/seafile/service_seafile.sh restart
+  else
+    echo "MYSQL_HOST is set, doing mysql install"
+    ./seafile-pro-server-${SEAFILE_VER}/setup-seafile-mysql.sh auto -n seafile
+  fi
+  if [ ! -f /opt/seafile/conf/admin.txt ]; then
+    echo '{"password": "password", "email": "admin@test.com"}' >> /opt/seafile/conf/admin.txt
+  fi
+}
+
+function do_upgrade() {
+  CURRENT_VER=$(ls -lah | grep 'seafile-server-latest' | awk -F"seafile-pro-server-" '{print $2}')
+  CURRENT_MAJOR_VER=$(echo $CURRENT_VER | awk -F"." '{print $1"."$2}')
+  NEW_MAJOR_VER=$(echo $SEAFILE_VER | awk -F"." '{print $1"."$2}')
+  if [ $CURRENT_VER == $SEAFILE_VER ]; then
+      echo "You already have the same version installed, exit"
+  else
+      download
+      cd ./seafile-pro-server-${SEAFILE_VER}/upgrade
+      echo current_version: $CURRENT_VER
+      echo new_version: ${SEAFILE_VER}
+      if [ $CURRENT_MAJOR_VER == $NEW_MAJOR_VER ]; then
+          echo "maintenance update"
+          ./minor-upgrade.sh
+          rm -rf /opt/seafile/seafile-pro-server-${CURRENT_VER}
+      else
+          echo "Big version update, Please run the matching upgrade script, and then run exit"
+          ls
+          su user
+      fi
+  fi
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  search_update)
+    stop_all
+    start_all
+    cd /opt/seafile/seafile-server-latest
     su user -c "./pro/pro.py search --clear <<EOF
-y
-EOF"
+  y
+  EOF"
     su user -c "./pro/pro.py search --update"
-elif [ "$1" == "clear" ]; then
-    cd ./seafile-server-latest
+    ;;
+  clear)
+    cd /opt/seafile/seafile-server-latest
     su user -c "./seahub.sh clearsessions"
-elif [ "$1" == "gc" ]; then
-    cd ./seafile-server-latest
+    ;;
+  gc)
+    cd /opt/seafile/seafile-server-latest
     su user -c "./seaf-gc.sh"
-elif [ "$1" == "fsck" ]; then
-    cd ./seafile-server-latest
+    ;;
+  fsck)
+    cd /opt/seafile/seafile-server-latest
     su user -c "./seaf-fsck.sh"
-elif [ "$1" == "bash" ]; then
-    #su user
+    ;;
+  shell)
     /bin/bash
-else
-    echo "invalid parameter"
-fi
+    ;;
+  *)
+    echo $"Usage: $0 {start|stop|restart|search_update|clear|gc|fsck|shell}"
+esac
+
+tail -F /opt/seafile/logs/seahub.log
